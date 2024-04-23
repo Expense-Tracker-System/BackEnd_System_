7fbf1859b47d10a96c0d258f6835c19c94e06b79
chinthakaSenarathna <chironprabath@gmail.com>
2024-04-23 16:27:26 +0530

    Visual StudioTest with Git

--- Core/Constants/StaticUserRoles.cs
+++ Core/Constants/StaticUserRoles.cs
@@ -3,7 +3,9 @@
     //This class will be used to avoid typing errors
     public static class StaticUserRoles
     {
+        public const string OWNER = "ADMIN";
         public const string ADMIN = "ADMIN";
+        public const string MANAGER = "ADMIN";
         public const string USER = "USER";
 
         public const string AdminUser = "ADMIN,USER";
