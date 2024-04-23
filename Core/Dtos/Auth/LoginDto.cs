0b5a1ad9ba7862e56acbe9981bda54c71680cb7b
chinthakaSenarathna <chironprabath@gmail.com>
2024-04-23 15:50:15 +0530

    initialized the backend system

---
+++ Core/Dtos/Auth/LoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend_dotnet7.Core.Dtos.Auth
+{
+    public class LoginDto
+    {
+        [Required(ErrorMessage = "UserName is Required")] //validation part in backend
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is Required")] //validation part in backend
+        public string Password { get; set; }
+        public string Mode { get; set; }
+    }
+}
