6c66b4b0b131d032a564d2f527726337a5664189
Ashen06C <ashencharanga99@gmail.com>
2024-06-06 16:12:53 +0530

    budget and reminder create

---
+++ backend-dotnet7/Core/Entities/Reminder.cs
@@ -0,0 +1,10 @@
+﻿namespace backend_dotnet7.Core.Entities
+{
+    public class Reminder : BaseEntity<long>
+    {
+        public int ReminderId {  get; set; }
+        public string ReminderName { get; set; }
+        public string ReminderDescription { get; set; }
+        public DateTime ReminderDate { get; set; } = DateTime.Now;
+    }
+}
