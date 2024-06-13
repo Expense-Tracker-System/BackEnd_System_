e1601ed8041a7b285f14e57990142eff3c0501b7
chinthakaSenarathna <chironprabath@gmail.com>
2024-06-14 02:13:54 +0530

    add UpdateUserImageDto -> IFormFile

---
+++ backend-dotnet7/Core/Dtos/Auth/UpdateUserImageDto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend_dotnet7.Core.Dtos.Auth
+{
+    public class UpdateUserImageDto
+    {
+        [Required(ErrorMessage = "UserName is Required")] //validation part in backend
+        public string Username { get; set; }
+        public IFormFile? ImageFile { get; set; }
+    }
+}
