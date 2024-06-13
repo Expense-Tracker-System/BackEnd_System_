4d7947fd10715a60fbbdc091eea81b3b36a970b5
chinthakaSenarathna <chironprabath@gmail.com>
2024-06-14 02:53:05 +0530

    set the Image services Interfaces

--- backend-dotnet7/Core/Interfaces/IUserImageService.cs
+++ backend-dotnet7/Core/Interfaces/IUserImageService.cs
@@ -1,6 +1,14 @@
-﻿namespace backend_dotnet7.Core.Interfaces
+﻿using backend_dotnet7.Core.Dtos.Auth;
+using backend_dotnet7.Core.Dtos.Image;
+
+namespace backend_dotnet7.Core.Interfaces
 {
     public interface IUserImageService
     {
+        Task<GetImageDto> AddUserImageAsync(AddUserImageDto addUserImageDto);
+        Task<GetImageDto> UpdateUserImageAsync(UpdateUserImageDto updateUserImageDto);
+        Task<GetImageDto> GetUserImageAsync();
+        Task<GetImageDto> FindUserImageByUsernameAsync(string userName);
+        Task DeleteUserImageAsync(string userName);
     }
 }
