4f4b705786a5352bfc95e853c2216b3f05e348fd
chinthakaSenarathna <chironprabath@gmail.com>
2024-06-14 02:19:10 +0530

    add AddUserImageDto

---
+++ backend-dotnet7/Core/Dtos/Auth/AddUserImageDto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend_dotnet7.Core.Dtos.Auth
+{
+    public class AddUserImageDto
+    {
+        [Required(ErrorMessage = "UserName is Required")] //validation part in backend
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "UserImage is Required")] //validation part in backend
+        public string? UserImage { get; set; }
+        public IFormFile? ImageFile { get; set; }
+    }
+}
