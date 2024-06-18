64e69546e76ef2e50528accac24020c2c3424307
chinthakaSenarathna <chironprabath@gmail.com>
2024-06-18 17:00:37 +0530

    create a realation for User -> Remainder & Budget

--- backend-dotnet7/Core/Entities/Reminder.cs
+++ backend-dotnet7/Core/Entities/Reminder.cs
@@ -1,12 +1,12 @@
 ﻿namespace backend_dotnet7.Core.Entities
 {
-    public class Reminder 
+    public class Reminder : BaseEntity<long>
     {
-        public int ReminderId {  get; set; }
-        public string ReminderName { get; set; }
+        public string? ReminderName { get; set; }
         public double ReminderAmount { get; set; }
-        public string ReminderDescription { get; set; }
+        public string? ReminderDescription { get; set; }
         public DateTime ReminderstartDate { get; set; }
         public DateTime ReminderendDate { get; set; }
+        public string? UserName { get; set; }
     }
 }
